8fb06d33aae831fd2d3dc5170b92e74b94b354e5
Rustam Achmetshin <achmetshinrust@gmail.com>
2026-03-20 14:34:23 +0700

    up
    up

---
+++ NewRPGGame2D/Assets/Scripts/Level/LayerSwitcher.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class LayerSwitcher : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sortingOrder = 2;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            spriteRenderer.sortingOrder = 25;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            spriteRenderer.sortingOrder = 2;
+        }
+    }
+}
