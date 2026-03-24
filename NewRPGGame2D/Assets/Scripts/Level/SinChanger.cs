27e74740d82145ad6477c49e8e8938e909feff09
Rustam Achmetshin <achmetshinrust@gmail.com>
2026-03-24 13:47:04 +0700

    up
    up

--- NewRPGGame2D/Assets/Scripts/Level/SinChanger.cs
+++ NewRPGGame2D/Assets/Scripts/Level/SinChanger.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class SinChanger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float amplitude = 1f;
+    [SerializeField] private float frequency = 1f;
+
+    float x;
+    float y;
+    float z;
+
+    private void Start()
     {
-        
+        x = transform.position.x;
+        y = transform.position.y;
+        z = transform.position.z;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float y1 = transform.position.y;
+
+        // Sine wave for vertical movement
+        y1 = Mathf.Sin(Time.time * frequency) * amplitude;
+
+        // Cosine wave for horizontal movement
+        //x = Mathf.Cos(Time.time * frequency) * amplitude;
+
+        transform.position = new Vector3(x, y + y1, z);
     }
 }
