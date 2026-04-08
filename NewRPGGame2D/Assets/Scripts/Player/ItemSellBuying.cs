2872851949232e4bcd820d6b39f2eacab4ba6479
RustamBerkut <achmetshinrust@gmail.com>
2026-04-08 22:46:07 +0700

    up
    up

--- NewRPGGame2D/Assets/Scripts/Player/ItemSellBuying.cs
+++ NewRPGGame2D/Assets/Scripts/Player/ItemSellBuying.cs
@@ -1,11 +1,15 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ItemSellBuying : MonoBehaviour, IDropHandler
 {
-    public int moneyForSell;
+    
     public static Action<int> OnItemSellAction;
+    public GameObject sellGO;
+
+    private int moneyForSell;
 
     public void OnDrop(PointerEventData eventData)
     {
@@ -13,6 +17,8 @@ public class ItemSellBuying : MonoBehaviour, IDropHandler
         moneyForSell = otherSlotTransform.GetComponent<UIItem>().ammoCost;
         moneyForSell /= (int)4;
         OnItemSellAction?.Invoke(moneyForSell);
+        GameObject selGO = Instantiate(sellGO);
+        selGO.GetComponentInChildren<TextMeshProUGUI>().text = string.Format("Продано за {0}", moneyForSell);
         Destroy(otherSlotTransform.gameObject);
     }
 }
