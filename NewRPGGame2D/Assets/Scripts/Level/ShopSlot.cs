cc2c3933553d494bf90f648011eb06850b9b80d5
RustamBerkut <achmetshinrust@gmail.com>
2026-04-08 21:01:56 +0700

    up
    up

--- NewRPGGame2D/Assets/Scripts/Level/ShopSlot.cs
+++ NewRPGGame2D/Assets/Scripts/Level/ShopSlot.cs
@@ -1,16 +1,25 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopSlot : MonoBehaviour
 {
+    [SerializeField]
+    private UIItem iItem;
+    [SerializeField]
+    private Image image;
+
     private int ammoCost;
     public static Action<int> MoneyForBuyingAction;
 
     private readonly string moneyName = "PlayerMoneyName";
     private int moneyPlayer;
 
+
     private void Start()
     {
+        image.sprite = iItem.GetComponent<Image>().sprite;
         OnGetPlayerMoney();
     }
     private void OnEnable()
@@ -24,6 +33,7 @@ public class ShopSlot : MonoBehaviour
     private void OnGetPlayerMoney()
     {
         moneyPlayer = PlayerPrefs.GetInt(moneyName);
+
     }
     public void OnBuyAmmo()
     {
@@ -41,5 +51,31 @@ public class ShopSlot : MonoBehaviour
     {
         var slot = GetComponentInChildren<UIItem>();
         ammoCost = slot.ammoCost;
+    }
+    [TextArea]
+    [SerializeField]
+    private string itemDescription;
+    private string itemCost;
+    public Texture2D icon;
+    private bool isDescr = false;
+
+    void OnGUI()
+    {
+        if (!isDescr) return;
+        GUIStyle guiStyle = new(GUI.skin.box)
+        {
+            fontSize = 35
+        };
+        guiStyle.normal.background = icon;
+        guiStyle.alignment = TextAnchor.MiddleCenter;
+        GUI.Box(new Rect(Screen.width / 2, Screen.height / 2 - 450, 500, 500), itemDescription, guiStyle);
+    }
+    public void OnMouseEnterItem()
+    {
+        isDescr = true;
+    }
+    public void OnMouseExitItem()
+    {
+        isDescr = false;
     }
 }
