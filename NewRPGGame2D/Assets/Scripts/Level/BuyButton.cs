f7290560559c412c9dadb0dfebe6185362940f33
Rustam Achmetshin <achmetshinrust@gmail.com>
2026-04-09 17:29:55 +0700

    up
    up

--- NewRPGGame2D/Assets/Scripts/Level/BuyButton.cs
+++ NewRPGGame2D/Assets/Scripts/Level/BuyButton.cs
@@ -1,17 +1,21 @@
+using NUnit.Framework;
+using PlayerBehaviour;
 using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class BuyButton : MonoBehaviour
 {
     public static Action<int> MoneyForBuyingAction;
-    public GameObject noMoneycanvas;
+    public GameObject noMoneycanvas, noCapacityCanvas;
 
     private readonly string moneyName = "PlayerMoneyName";
     private int moneyPlayer;
     private LootingItem LootingItem;
 
     private int ammoCost;
+    public List<InventorySlot> UIItems;
 
     public void OnGetPlayerMoney(int cost, LootingItem looting)
     {
@@ -20,17 +24,29 @@ public class BuyButton : MonoBehaviour
     }
     public void OnBuyAmmo()
     {
-        if (LootingItem == null) return;
-        moneyPlayer = PlayerPrefs.GetInt(moneyName);
-        if (ammoCost <= moneyPlayer)
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+        UIItems = player.gameObject.GetComponent<PlayerInventory>().UIslots;
+        for (int i = 0; i < UIItems.Count; i++)
         {
-            MoneyForBuyingAction?.Invoke(ammoCost * -1);
-            Transform player = GameObject.FindGameObjectWithTag("Player").transform;
-            Instantiate(LootingItem, player.position, player.rotation);
-        }
-        else
-        {
-            Instantiate(noMoneycanvas);
+            if (UIItems[i].transform.childCount == 0)
+            {
+                if (LootingItem == null) return;
+                moneyPlayer = PlayerPrefs.GetInt(moneyName);
+                if (ammoCost <= moneyPlayer)
+                {
+                    MoneyForBuyingAction?.Invoke(ammoCost * -1);
+                    Instantiate(LootingItem, player.position, player.rotation);
+                }
+                else
+                {
+                    Instantiate(noMoneycanvas);
+                }
+                break;
+            }
+            else
+            {
+                //Instantiate(noCapacityCanvas);
+            }
         }
     }
 }
