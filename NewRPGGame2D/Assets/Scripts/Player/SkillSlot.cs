c6070b5c424036288831a1d8901223e70b8e19e6
Rustam Achmetshin <achmetshinrust@gmail.com>
2026-04-13 10:39:55 +0700

    up
    up

---
+++ NewRPGGame2D/Assets/Scripts/Player/SkillSlot.cs
@@ -0,0 +1,97 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SkillSlot : MonoBehaviour, IDropHandler
+{
+    public List<Resours> resours;
+    public List<GameObject> equipmentSlotOnPlayer;
+
+    public string equipmentSlotName;
+    public SkillButton skillButton;
+
+    private void Start()
+    {
+        OnLoadItemInSlot();
+    }
+    private void OnEnable()
+    {
+        UIItem.UpdateItemStatAction += OnUpdateSlot;
+    }
+    private void OnDisable()
+    {
+        UIItem.UpdateItemStatAction -= OnUpdateSlot;
+        if (transform.childCount != 0)
+        {
+            string value = gameObject.GetComponentInChildren<UIItem>().itemName;
+            OnSaveItemInSlot(value);
+        }
+        if (transform.childCount == 0)
+        {
+            OnClearItemSlot();
+        }
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (transform.childCount == 0)
+        {
+            var otherSlotTransform = eventData.pointerDrag.transform;
+            Resours res = otherSlotTransform.GetComponent<UIItem>().resours;
+            int number = otherSlotTransform.GetComponent<UIItem>().numberOfItem;
+            foreach (var item in resours)
+            {
+                if (item == res)
+                {
+                    otherSlotTransform.SetParent(transform);
+                    otherSlotTransform.localPosition = Vector3.zero;
+                    otherSlotTransform.localScale = Vector3.one;
+                    foreach (var slot in equipmentSlotOnPlayer)
+                    {
+                        slot.GetComponent<EquipmentList>().SetupItemInSlot(number);
+                    }
+                }
+                else Debug.Log("net + {0}");
+            }
+        }
+        else
+        {
+            Debug.Log("zanyto");
+        }
+    }
+    private void OnUpdateSlot()
+    {
+        if (transform.childCount == 0)
+        {
+            foreach (var slot in equipmentSlotOnPlayer)
+            {
+                slot.GetComponent<EquipmentList>().SetupItemInSlot(0);
+                OnClearItemSlot();
+            }
+        }
+    }
+    private void OnLoadItemInSlot()
+    {
+        if (!PlayerPrefs.HasKey(equipmentSlotName)) return;
+
+        string value = PlayerPrefs.GetString(equipmentSlotName);
+        value = string.Format("Loot/{0}", value);
+        Debug.Log(value);
+        var it = (GameObject)Instantiate(Resources.Load(value));
+        it.transform.SetParent(transform);
+        it.transform.localPosition = Vector3.zero;
+        it.transform.localScale = Vector3.one;
+        int number = it.GetComponent<UIItem>().numberOfItem;
+        foreach (var slot in equipmentSlotOnPlayer)
+        {
+            slot.GetComponent<EquipmentList>().SetupItemInSlot(number);
+        }
+    }
+    private void OnSaveItemInSlot(string item)
+    {
+        PlayerPrefs.SetString(equipmentSlotName, item);
+    }
+    private void OnClearItemSlot()
+    {
+        PlayerPrefs.DeleteKey(equipmentSlotName);
+    }
+}
